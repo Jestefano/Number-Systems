33ea22458e271ff8e0520ecce8e5cb8cc7696a84
Jesus Torres <jstorres@pucp.pe>
2019-05-09 20:50:52 -0500

    Documentation of functions

--- number_systems.py
+++ number_systems.py
@@ -1,6 +1,7 @@
 
 class fraction():
 	""" 
+	This is the implementation of a fraction number
 	Note: To work properly we assume that there is no other class
 		called fraction neither in the project nor in the modules that
 		the user has imported. Otherwise, all the operations will not
@@ -9,6 +10,8 @@ class fraction():
 	frac_count = 1
 
 	def __init__(self,numerator,denominator):
+		"""In order to initialize we ask for two integer numbers
+		The second one should be different from zero """
 		assert (type(numerator)==int), "Numerator has to be int"
 		assert (type(denominator)==int), "Denominator has to be int"
 		assert (denominator != 0), "Denominator can't be zero"
@@ -20,12 +23,13 @@ class fraction():
 		fraction.frac_count += 1
 
 	def __str__(self):
+		"""We print the number as a/b"""
 		return (str(self.numerator)+'/'+str(self.denominator))
 
 	def __add__(self,other):
 		"""
 		Addition:
-		Receive as parameter either a fraction or an integer
+		We can add with a fraction or an integer
 		"""
 		if('fraction' in str(type(other))):
 			new_numerator = self.numerator*other.denominator + other.numerator*self.denominator
@@ -40,7 +44,9 @@ class fraction():
 			raise Exception("Use an integer or fraction in the operator")
 
 	def __sub__(self,other):
-		"""This is a temporal comment about the implementation of sub """
+		"""
+		We can substract with another fraction or an integer
+		"""
 		if('fraction' in str(type(other))):
 			new_frac = fraction((-1)*other.get_numerator(),other.get_denominator())
 			return fraction.__add__(self,new_frac)
@@ -51,7 +57,9 @@ class fraction():
 			raise Exception("Use an integer or fraction in the operator")
 
 	def __mul__(self,other):
-
+		"""
+		We can multiply with another fraction or an integer
+		"""
 		if('fraction' in str(type(other))):
 			new_numerator = self.numerator * other.get_numerator()
 			new_denominator = self.denominator * other.get_denominator()
@@ -65,6 +73,10 @@ class fraction():
 			raise Exception("Use an integer or fraction in the operator")
 
 	def __truediv__(self,other):
+		"""
+		We can divide with another fraction or an integer as long as they
+		are not zero
+		"""
 		if('fraction' in str(type(other))):
 			assert (other.get_numerator()!=0), "Can't divide by zero"
 			new_numerator = self.numerator * other.get_denominator()
@@ -80,15 +92,23 @@ class fraction():
 			raise Exception("Use an integer or fraction in the operator")
 
 	def invert(self):
+		"""
+		We can invert a non-zero fraction """
 		assert (self.numerator!=0), "Numerator can't be zero"
 
 		self.numerator,self.denominator = self.denominator,self.numerator
 		return self
 
 	def __gcd(a,b):
+		"""
+		This is an internal function that we use to reduce 
+		the fraction """
 		return (b if a%b==0 else fraction.__gcd(b,a%b))
 
 	def reduce(self):
+		"""
+		This function returns the fraction reduced 
+		"""
 		gcd_ = fraction.__gcd(self.numerator,self.denominator)
 		self.numerator = self.numerator//gcd_
 		self.denominator = self.denominator//gcd_
@@ -103,6 +123,9 @@ class fraction():
 	def get_real_value(self):
 		return self.numerator/self.denominator
 
+	def get_frac_count(self):
+		return self.frac_count
+
 	def set_numerator(self,new_numerator):
 		assert (type(new_numerator)==int), "Numerator has to be int"
 		self.numerator = new_numerator
@@ -113,14 +136,17 @@ class fraction():
 
 		self.denominator = new_denominator
 
-	def get_frac_count(self):
-		return self.frac_count
 
 
 class integer(fraction):
+	"""This is my implementation of an integer number
+	It is based on the int class from Python, and it supports
+	operations with integer and fraction """
 	int_count = 1
 
 	def __init__(self,value):
+		"""
+		In order to initialize this we need an int value"""
 		assert (type(value)==int), "Value should be integer"
 
 		fraction.__init__(self,value,1)
@@ -129,10 +155,13 @@ class integer(fraction):
 		integer.int_count += 1
 
 	def __str__(self):
+		""" I print the value itself"""
 		return str(self.value)
+		
 
 	def __add__(self,other):
-
+		"""We can add an integer with a integer or
+		with a fraction """
 		if("fraction" in str(type(other))):
 			new_frac = fraction.__add__(other,self)
 			new_frac.reduce()
@@ -148,6 +177,8 @@ class integer(fraction):
 			raise Exception("Integer or fraction expected")
 
 	def __sub__(self,other):
+		"""We can substract an integer with a integer or
+		with a fraction """
 		if("fraction" in str(type(other))):
 			aux_frac = fraction(-other.get_numerator(),other.get_denominator())
 			new_frac = fraction.__add__(aux_frac,self)
@@ -164,7 +195,8 @@ class integer(fraction):
 			raise Exception("Integer or fraction expected")
 
 	def __mul__(self,other):
-
+		"""We can multiply an integer with a integer or
+		with a fraction """
 		if("fraction" in str(type(other))):
 			new_frac = fraction.__mul__(other,self)
 			return new_frac
@@ -175,6 +207,8 @@ class integer(fraction):
 			raise Exception("Operator has to be integer or fraction")
 
 	def __truediv__(self,other):
+		"""We can divide an integer with a integer or
+		with a fraction as long as it is non-zero"""
 		if("integer" in str(type(other))):
 			assert (other.get_value()!=0), "Can't divide by zero"
 			new_frac = fraction(self.value,other.get_value())
@@ -193,6 +227,8 @@ class integer(fraction):
 			raise Exception("Operators have to be integer or fraction")
 
 	def __floordiv__(self,other):
+		"""This function returns the floor division. It only
+		accepts an integer as parameter"""
 		assert ("integer" in str(type(other))), "Operand has to be integer"
 		assert (other.value!=0), "Can't divide by zero"
 
@@ -201,7 +237,6 @@ class integer(fraction):
 
 	def set_value(self,value):
 		assert (type(value)==int), "Value should be integer"
-
 		self.value = value
 
 	def get_value(self):
